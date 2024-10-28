ea1d461a50125c0a9c71c7536f3fc22da719ce5c
tonii <152874628+bL1nkd@users.noreply.github.com>
2024-10-28 21:16:48 +0800

    Add files via upload

---
+++ ScientificCalcu/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScientificCalcu
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HandleInput ui = new HandleInput();
+            ui.Start();
+        }
+    }
+}
---
+++ ScientificCalcu/ScientificOperations.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ScientificCalcu
+{
+    //class for scientific
+    public class ScientificOperations
+    {
+        public double SquareRoot(double a) { return Math.Sqrt(a); }
+        public double Power(double a, double b) { return Math.Pow(a, b); }
+    }
+}
